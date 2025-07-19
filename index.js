f986c761e92d2c2694dda8c21df871455aad46f7
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-07-19 17:40:22 +0600

    Firebase Admin Sdk setup done

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 dotenv.config();
+var admin = require("firebase-admin");
 
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY); 
 
@@ -12,6 +13,14 @@ const port = process.env.PORT || 5000;
 // Middlewares
 app.use(cors());
 app.use(express.json());
+
+// Firebase Setup
+var serviceAccount = require("./sports-club-management-firebase-adminsdk.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
 
 // MongoDB Setup
 const uri = process.env.MONGO_URI;
